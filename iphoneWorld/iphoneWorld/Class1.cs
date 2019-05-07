416edf97c4f2ca4f3ae5f22829867a4f2a1724b9
XHBLX <37951357+XHBLX@users.noreply.github.com>
2019-05-08 01:27:55 +0800

    Update Class1.cs

--- iphoneWorld/iphoneWorld/Class1.cs
+++ iphoneWorld/iphoneWorld/Class1.cs
@@ -9,24 +9,25 @@ namespace demo.iphoneWorld
     public class Iphone : testItem
     {
         bool broken;
-        int maxiFloor;
-        int currentFloor;
+        int maxiFloor; //why would a phone know the max floor, max of what
+        int currentFloor; //makes sense to be at current floor
         IfloorTellable Carrier;
         public void breakNow(){}
         public void getTested() { }
-        public void movedToNewFloor() { }
+        public void movedToNewFloor() { } 
         public void carriedBy(Object carrier) { }
     }
 
     public class DPAlgorithmGenerator : IalgoGeneratable
     {
         Itable DPTable;//I think the algo need to write data to this table?
-        public void generateDPAlgo() { }
+        public void generateDPAlgo() { } //and where is the algorithm if you don't return it?
     }
 
     public class Engineer: Tester
     {
-        //A list of <ITestable>
+        //A list of <ITestable>  
+        //why comment out
         int currentFloor;
         IalgoGeneratable algo;
         public void tellfloor() { }
@@ -38,6 +39,7 @@ namespace demo.iphoneWorld
     public class buidling : Ibuilding
     {
         // The engineer is in the building, so injection?
+        //sure
         Imovable engineerInThisBuilding;
         int height;
     }
